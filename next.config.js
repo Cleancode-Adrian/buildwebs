1b7a0cba64fdc4c6ee5b2c7bc6d24bb52b1b4731
BuildWebs <contact@buildwebs.eu>
2025-11-11 15:24:45 +0100

    Fix: Remove optimizeCss - causing build errors

--- next.config.js
+++ next.config.js
@@ -28,7 +28,6 @@ const nextConfig = {
 
   // Experimental features for better performance
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ['react-icons', 'framer-motion'],
   },
 
